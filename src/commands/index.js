f9f32ab7e9ee55f40ca3cea7c221c537d44b37d9
edmilson-dk <droidvidaboa@gmail.com>
2021-02-26 11:43:26 -0300

    feat: created commands module

---
+++ src/commands/index.js
@@ -0,0 +1,23 @@
+const { init_cmd } = require('../messages/commands.json');
+const { init_btn } = require('../messages/inline_keyboard.json');
+const {
+  getChatId,
+} = require('../helpers/command_help');
+
+module.exports = bot => {
+  bot.command('init', async ctx => {
+    const { type } = await ctx.getChat();
+
+    if (type === 'private') {
+      ctx.telegram.sendMessage(getChatId(ctx), init_cmd.text, {
+        reply_markup: {
+          inline_keyboard: [
+            [{ text: init_btn.help.text, callback_data: 'help'}],
+            [{ text: init_btn.add_bot.text, url: `http://t.me/${process.env.BOT_USERNAME}?startgroup=botstart`}],
+            [{ text: init_btn.add_feed.text, callback_data: 'add_feed'}],
+          ]
+        }
+      });
+    }
+  })
+}
