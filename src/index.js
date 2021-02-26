269e1c3c0cf49cf73dd58d7bcf074188457c4ac5
edmilson-dk <droidvidaboa@gmail.com>
2021-02-26 17:00:47 -0300

    feat: added command module and start bot handler

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 require('dotenv').config();
 const { Telegraf } = require('telegraf');
 
-
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start(ctx => {
-  ctx.telegram.sendMessage(ctx.chat.id, 'Olá! digite o comando /init para ver meu menu de opções.');
+const commands = require('./commands');
+
+bot.start(async ctx => {
+  const { type } = await ctx.getChat();
+ 
+  type === 'private' 
+    ? ctx.telegram.sendMessage(ctx.chat.id, 'Olá! digite o comando /init para ver meu menu de opções.')
+    : ctx.telegram.sendMessage(ctx.chat.id, 'Olá!');  
 })
 
-const commands = require('./commands');
-//
 commands(bot);
 
 bot.launch();
