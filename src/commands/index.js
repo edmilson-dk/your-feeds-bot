c283cb9817dee7ae083c8c4aadcf6483cfc54b41
edmilson-dk <droidvidaboa@gmail.com>
2021-02-26 16:08:48 -0300

    feat: removed actions and moved to other folder

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,10 +1,10 @@
 const { init_cmd } = require('../messages/commands.json');
-const { init_btn, go_back_btn } = require('../messages/inline_keyboard.json');
-const { setup_feed, help, add_super_chat, add_bot } = init_btn;
+const { init_btn } = require('../messages/inline_keyboard.json');
+const { getChatId } = require('../helpers/command_help');
 
-const {
-  getChatId,
-} = require('../helpers/command_help');
+const actions = require('../actions/setup_feed_actions');
+
+const { setup_feed, help } = init_btn;
 
 module.exports = bot => {
   const initMarkup =  {
@@ -16,7 +16,6 @@ module.exports = bot => {
     },
   }
 
-
   bot.command('init', async ctx => {
     const { type } = await ctx.getChat();
 
@@ -25,38 +24,6 @@ module.exports = bot => {
     }
   })
 
-  bot.action('init_menu', ctx => {
-    ctx.answerCbQuery();
-    ctx.deleteMessage();
-    ctx.telegram.sendMessage(getChatId(ctx), init_cmd.text, initMarkup);
-  }) 
-
-  bot.action('setup_feed', ctx => {
-    ctx.answerCbQuery();
-    ctx.deleteMessage();
-
-    ctx.telegram.sendMessage(getChatId(ctx), setup_feed.action_text, {
-      reply_markup: {
-        inline_keyboard: [
-          [{ text: setup_feed.action_super_chat, callback_data: 'action_super_chat'}],
-          [{ text: setup_feed.action_private_chat, callback_data: 'action_private_chat' }],
-          [{ text: go_back_btn.text, callback_data: 'init_menu' }]
-        ]
-      }
-    })
-  })
-
-  bot.action('action_super_chat', ctx => {
-    ctx.answerCbQuery();
-    ctx.deleteMessage();
-
-    ctx.telegram.sendMessage(getChatId(ctx), add_super_chat.text, {
-      reply_markup: {
-        inline_keyboard: [
-          [{ text: go_back_btn.text, callback_data: 'setup_feed' }]
-        ]
-      }
-    })
-  })
+  actions(bot, initMarkup);
 }
 
