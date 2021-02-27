97ff2c9fd3335b0e0eaa962ef0ec69772e275859
edmilson-dk <droidvidaboa@gmail.com>
2021-02-27 12:21:15 -0300

    fix: corrected the name of the callback actions

--- src/actions/setup_feed_actions.js
+++ src/actions/setup_feed_actions.js
@@ -2,7 +2,7 @@ const { start_bot } = require('../messages/commands.json');
 const { start_bot_keyboard, go_back_btn } = require('../messages/inline_keyboard.json');
 const { getChatId } = require('../helpers/bot_helpers');
 
-const { setup_feed, add_super_chat } = start_bot_keyboard;
+const { manager_feed, add_super_chat } = start_bot_keyboard;
 
 module.exports = (bot, initMarkup) => {
   bot.action('start_bot', ctx => {
@@ -11,15 +11,14 @@ module.exports = (bot, initMarkup) => {
     ctx.telegram.sendMessage(getChatId(ctx), start_bot.text, initMarkup);
   }) 
 
-  bot.action('setup_feed', ctx => {
+  bot.action('manager_feed', ctx => {
     ctx.answerCbQuery();
     ctx.deleteMessage();
 
-    ctx.telegram.sendMessage(getChatId(ctx), setup_feed.action_text, {
+    ctx.telegram.sendMessage(getChatId(ctx), manager_feed.action_text, {
       reply_markup: {
         inline_keyboard: [
-          [{ text: setup_feed.action_super_chat, callback_data: 'action_super_chat'}],
-          [{ text: setup_feed.action_private_chat, callback_data: 'action_private_chat' }],
+          [{ text: manager_feed.action_super_chat, callback_data: 'action_super_chat'}],
           [{ text: go_back_btn.text, callback_data: 'start_bot' }]
         ]
       }
@@ -33,7 +32,7 @@ module.exports = (bot, initMarkup) => {
     ctx.telegram.sendMessage(getChatId(ctx), add_super_chat.text, {
       reply_markup: {
         inline_keyboard: [
-          [{ text: go_back_btn.text, callback_data: 'setup_feed' }]
+          [{ text: go_back_btn.text, callback_data: 'manager_feed' }]
         ]
       }
     })
