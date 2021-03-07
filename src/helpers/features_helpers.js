f14acd58a0bfc17c104bef1d0dea700ff6d7c941
edmilson-dk <droidvidaboa@gmail.com>
2021-03-07 12:07:59 -0300

    feat: created news features helpers

--- src/helpers/features_helpers.js
+++ src/helpers/features_helpers.js
@@ -4,6 +4,26 @@ async function asyncFilter(array, cb) {
   return array.filter((_, index) => results[index]);
 }
 
+function isHashtagsValid(hashtags) {
+  if (hashtags.length === 0) return false;
+
+  const hashtagsValids = hashtags.filter(hashtag => hashtag[0] === '#');
+  
+  return hashtagsValids.length < hashtags.length 
+    ? false
+    : true;
+}
+
+function removeSpacesInArray(array) {
+  if (array.length === 0) return;
+
+  const newArray = array.filter(item => item !== '');
+
+  return newArray;
+}
+
 module.exports = {
   asyncFilter,
+  isHashtagsValid,
+  removeSpacesInArray,
 }
