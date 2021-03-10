684dbcea35406e9c806e941caf788c304b5fcee8
edmilson-dk <droidvidaboa@gmail.com>
2021-03-10 09:17:26 -0300

    refactor: renamed the feeds table name to feeds

--- src/infra/database/query-builder/postgres/knex/migrations/20210227120105_feed_table.js
+++ src/infra/database/query-builder/postgres/knex/migrations/20210227120105_feed_table.js
@@ -1,14 +1,14 @@
 exports.up = function(knex) {
-  knex.schema.hasTable('feed').then(exists => {
+  knex.schema.hasTable('feeds').then(exists => {
     if (!exists) {
-      return knex.schema.createTable('feed', table => {
+      return knex.schema.createTable('feeds', table => {
         table.string('rss_url').notNullable();
         table.string('hashtag').notNullable();
         table.string('title', 60).notNullable();
 
         // relation to chat table
         table.string('chat_id', 60)
-          .references('chat.id')
+          .references('chats.id')
           .notNullable()
           .onDelete('CASCADE');
         
@@ -19,5 +19,5 @@ exports.up = function(knex) {
 };
 
 exports.down = function(knex) {
-  knex.schema.dropTableIfExists('feed');
+  knex.schema.dropTableIfExists('feeds');
 };
