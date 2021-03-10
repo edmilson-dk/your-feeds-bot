d6eabe5ef15c68871a35824c184aa5f4b10b8108
edmilson-dk <droidvidaboa@gmail.com>
2021-03-10 09:12:56 -0300

    feat: created set time command

--- src/commands/index.js
+++ src/commands/index.js
@@ -3,10 +3,12 @@ const {
   home, cmd_error, not_member_admin, 
   view_chat, add_feed, remove_feed, times_table
 } = require('../messages/commands');
-
 const { homeMarkup, timezonesMarkup, isNotMemberOrAdminMarkup, goBackManagerFeedsMarkup } = require('../markups');
-const { getChatId, isBotAdmin, isAdmin, isStillMemberAndAdmin } = require('../helpers/bot_helpers');
-const { asyncFilter, isHashtagsValid, removeSpacesInArray, removeNotHashtagsInArray, listFeeds } = require('../helpers/features_helpers');
+const { getChatId, isBotAdmin, isAdmin, isStillMemberAndAdmin, removeCommand } = require('../helpers/bot_helpers');
+const {
+   asyncFilter, isHashtagsValid, removeSpacesInArray,
+   removeNotHashtagsInArray, listFeeds, validateTimes, formatTimes
+} = require('../helpers/features_helpers');
 
 const User = require('../domain/User');
 const Feed = require('../domain/Feed');
@@ -155,6 +157,7 @@ module.exports = bot => {
 
       bot.command('add', async ctx => await addFeedCommand(ctx, chatID, getMessage));
       bot.command('remove', async ctx => await removeFeedCommand(ctx, chatID, getMessage));
+      bot.command('setTime', async ctx => await setTimeCommand(ctx, chatID, getMessage));
 
     } else {
       ctx.telegram.sendMessage(chatID, not_member_admin.text, isNotMemberOrAdminMarkup);
@@ -165,11 +168,16 @@ module.exports = bot => {
   })
 
   async function addFeedCommand(ctx, chat_id, getMessage) {
-    let data = ctx.message.text.replace('/add', '').trim().split(' ');
+    let data = removeCommand(ctx.message.text, '/add').split(' ');
     data = removeSpacesInArray(data);
 
     const rssURL = data.slice(0, 1).toString();
     const hashtags = removeNotHashtagsInArray(data);
+
+    if (hashtags.length > 3) {
+      ctx.telegram.sendMessage(getChatId(ctx), '⚠️ Você deve enviar no máximo 3 hashtags.');
+      return;
+    }
 
     if (!isHashtagsValid(hashtags)) {
       ctx.telegram.sendMessage(getChatId(ctx), add_feed.cmd_error, { parse_mode: 'HTML'});
@@ -186,7 +194,7 @@ module.exports = bot => {
       }
 
       const feed = new Feed(rssURL, hashtags_formatted, title, chat_id);
-      await feedRepository.addFeed(...feed.getValue());
+      await feedRepository.addFeed(feed.getValue());
 
       ctx.reply(add_feed.success);
       const message = await getMessage();
@@ -200,7 +208,7 @@ module.exports = bot => {
   }
 
   async function removeFeedCommand(ctx, chat_id, getMessage) {
-    const feedTitle = ctx.message.text.replace('/remove', '').trim();
+    const feedTitle = removeCommand(ctx.message.text, '/remove');
 
     if (!feedTitle) {
       ctx.telegram.sendMessage(getChatId(ctx), remove_feed.cmd_error, { parse_mode: 'HTML'});
@@ -220,12 +228,47 @@ module.exports = bot => {
     return;
   }
 
+  async function setTimeCommand(ctx, chat_id, getMessage) {
+    const values = removeCommand(ctx.message.text, '/setTime').split(' ');
+
+    if (values.length < 2) {
+      ctx.reply('⚠️ Por favor, envie o comando com todos os dados corretamente.');
+      return;
+    }
+
+    const [ startPostsTime, endPostsTime ] = values;
+
+    if (!validateTimes(startPostsTime)) {
+      ctx.reply('⚠️ Formato do horário de início das postagens inválido.');
+      return;
+    }
+    if (!validateTimes(endPostsTime)) {
+      ctx.reply('⚠️ Formato do horário de termino das postagens inválido.');
+      return;
+    }
+    
+    let validStartPostsTime = formatTimes(startPostsTime);
+    let validEndPostsTime = formatTimes(endPostsTime);
+  
+    await chatRepository.updateTimesChat({
+      chat_id,
+      start_posts: validStartPostsTime,
+      end_posts: validEndPostsTime 
+      });
+
+    ctx.reply('✅ Horários definidos com sucesso.');
+    const message = await getMessage();
+    ctx.telegram.sendMessage(...message);
+
+    return;
+  }
+
   bot.command('times_table', ctx => {
-    const { format, time_end, time_start, intervals } = times_table;
+    const { format, time_end, time_start } = times_table;
 
     ctx.telegram.sendMessage(
       getChatId(ctx), 
-    `${format}\n\n${time_start}\n\n${time_end}\n\n${intervals}`, 
+    `${format}\n\n${time_start}\n\n${time_end}`, 
      goBackManagerFeedsMarkup);
 
     return;
