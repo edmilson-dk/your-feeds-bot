c4b3925d8cbcb92321d06056ce09b921513e72e3
edmilson-dk <droidvidaboa@gmail.com>
2021-03-11 16:06:58 -0300

    feat: created post repositorys

---
+++ src/infra/repositories/post_repository.js
@@ -0,0 +1,25 @@
+const knex = require('../database/query-builder/postgres/knex');
+
+class PostRepository {
+  async addPost({ title, chat_id }) {
+    await knex('posts')
+      .insert({ title, chat_id });
+  }
+
+  async existsPost({ title, chat_id }) {
+    const exists = await knex('posts')
+      .where({ title, chat_id });
+
+    return exists.length > 0 ? true : false;
+  }
+
+  async getPostsCount({ chat_id }) {
+    const count = await knex('posts')
+      .where({ chat_id })
+      .count()
+
+    return count[0];
+  }
+}
+
+module.exports = PostRepository;
