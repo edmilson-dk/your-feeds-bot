ad18974294e50688b25a23dc04c23de9d8ae91b6
edmilson-dk <droidvidaboa@gmail.com>
2021-03-11 08:09:10 -0300

    feat: added new column in chats table

--- src/infra/database/query-builder/postgres/knex/migrations/20210227120048_chat_table.js
+++ src/infra/database/query-builder/postgres/knex/migrations/20210227120048_chat_table.js
@@ -6,6 +6,7 @@ exports.up = function(knex) {
         table.string('title', 100).notNullable();
         table.string('start_posts', 20).notNullable();
         table.string('end_posts', 20).notNullable();
+        table.boolean('active').defaultTo(false);
 
         // relation to user table
         table.string('user_id')
