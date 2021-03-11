408804d928eeb633ffbaa6a238655321a68b335c
edmilson-dk <droidvidaboa@gmail.com>
2021-03-11 16:01:27 -0300

    feat: added core method init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 
 const { Telegraf } = require('telegraf');
-
+const init = require('./core');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const commands = require('./commands');
 
 commands(bot);
+setTimeout(async () => await init(bot), 0);
 
 bot.launch();
 process.once('SIGINT', () => bot.stop('SIGINT'));
