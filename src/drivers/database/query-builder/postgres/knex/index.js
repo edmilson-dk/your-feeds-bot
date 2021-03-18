24e1bc3cf2330da5cb040695251e1edcc74e35a4
edmilson-dk <droidvidaboa@gmail.com>
2021-03-18 17:48:12 -0300

    fix: removed start service command error

--- src/drivers/database/query-builder/postgres/knex/index.js
+++ src/drivers/database/query-builder/postgres/knex/index.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 const environment = process.env.NODE_ENV || 'development';
 
 const knexConfig = require('../../../../../../knexfile')[environment];
 const knex = require('knex')(knexConfig);
-=======
-const knexfile = require('../../../../../../knexfile');
-const knex = require('knex')(knexfile.production);
->>>>>>> 41ebc27b67b284c272f2c03fc54569e1197c4b6f
 
 module.exports = knex;
